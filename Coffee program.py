293e0230c976e7839594d088cc4065ff8309c8a4
Luxray-yay <elisabeta.bayer@kingsway.school.nz>
2025-10-10 10:55:51 +1300

    Ver 2

--- Coffee program.py
+++ Coffee program.py
@@ -7,15 +7,32 @@
 #       Record the answer
 #       Give a response back to the user
 
+# Versio 1
 # Ask the user whether they lke coffee or not
-like_coffee = input("Do you like coffee? ")
+'''like_coffee = input("Do you like coffee? ")
 print(f'Your answer was "{like_coffee}".')
 
 if like_coffee == "yes" or "Yes" or "y" or "yep":
     print("That is great! I like coffee too.")
 
 else:
-    print("You are missing out! Why not give it a try?")
+    print("You are missing out! Why not give it a try?")'''
 
+# Version 2
+# While loop
+keep_going = ""
+while keep_going == "":
+    like_coffee = input("Do you like coffee?")
 
+    if like_coffee == "yes" or like_coffee == "Yes" or like_coffee == "y" or like_coffee "yep":
+         print("That is great! I like coffee too.")
+     
+    elif like_coffee == "no" or "No" or "n":
+         print("You are missing out! Why not give it a try?") 
+
+else:
+     print("I don't understand. ")
+
+            
+    
 
